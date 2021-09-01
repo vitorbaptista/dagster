bb1e15a12effa024655241e86da1aa4925c3a417
Sandy Ryza <sandy@elementl.com>
2021-09-01 10:15:16 -0700

    migrate dbt docs to graph/job/op

--- examples/docs_snippets_crag/docs_snippets_crag/integrations/dbt.py
+++ examples/docs_snippets_crag/docs_snippets_crag/integrations/dbt.py
@@ -13,47 +13,51 @@ def scope_dbt_cli_resource_config():
 
 def scope_dbt_cli_run():
     # start_marker_dbt_cli_run_preconfig
-    from dagster import pipeline, solid, ModeDefinition
+    from dagster import graph, op
     from dagster_dbt import dbt_cli_resource
 
     my_dbt_resource = dbt_cli_resource.configured({"project_dir": "path/to/dbt/project"})
 
-    @solid(required_resource_keys={"dbt"})
+    @op(required_resource_keys={"dbt"})
     def run_all_models(context):
         context.resources.dbt.run()
 
-    @pipeline(mode_defs=[ModeDefinition(resource_defs={"dbt": my_dbt_resource})])
-    def my_dbt_pipeline():
+    @graph
+    def my_dbt_graph():
         run_all_models()
+
+    my_dbt_job = my_dbt_graph.to_job(resource_defs={"dbt": my_dbt_resource})
 
     # end_marker_dbt_cli_run_preconfig
 
 
 def scope_dbt_cli_run_specific_models():
     # start_marker_dbt_cli_run_specific_models_preconfig
-    from dagster import pipeline, solid, ModeDefinition
+    from dagster import graph, op
     from dagster_dbt import dbt_cli_resource
 
     my_dbt_resource = dbt_cli_resource.configured(
         {"project_dir": "path/to/dbt/project", "models": ["tag:staging"]}
     )
 
-    @solid(required_resource_keys={"dbt"})
+    @op(required_resource_keys={"dbt"})
     def run_models(context):
         context.resources.dbt.run()
 
-    @pipeline(mode_defs=[ModeDefinition(resource_defs={"dbt": my_dbt_resource})])
-    def my_dbt_pipeline():
+    @graph
+    def my_dbt_graph():
         run_models()
+
+    my_dbt_job = my_dbt_graph.to_job(resource_defs={"dbt": my_dbt_resource})
 
     # end_marker_dbt_cli_run_specific_models_preconfig
 
 
 def scope_dbt_cli_run_specific_models_runtime():
     # start_marker_dbt_cli_run_specific_models_runtime
-    from dagster import solid
+    from dagster import op
 
-    @solid(required_resource_keys={"dbt"})
+    @op(required_resource_keys={"dbt"})
     def run_models(context, some_condition: bool):
         if some_condition:
             context.resources.dbt.run(models=["tag:staging"])
@@ -65,61 +69,60 @@ def scope_dbt_cli_run_specific_models_runtime():
 
 def scope_dbt_cli_profile_modes():
     # start_marker_dbt_cli_profile_modes
-    from dagster import pipeline, solid, ModeDefinition
+    from dagster import graph, op
     from dagster_dbt import dbt_cli_resource
 
-    @solid(required_resource_keys={"dbt"})
+    @op(required_resource_keys={"dbt"})
     def run_all_models(context):
         context.resources.dbt.run()
 
-    @pipeline(
-        mode_defs=[
-            ModeDefinition(
-                "dev",
-                resource_defs={
-                    "dbt": dbt_cli_resource.configured(
-                        {"project_dir": "path/to/dbt/project", "profile": "dev"}
-                    )
-                },
-            ),
-            ModeDefinition(
-                "prod",
-                resource_defs={
-                    "dbt": dbt_cli_resource.configured(
-                        {"project_dir": "path/to/dbt/project", "profile": "prod"}
-                    )
-                },
-            ),
-        ]
-    )
-    def my_dbt_pipeline():
+    @graph
+    def my_dbt_graph():
         run_all_models()
+
+    my_dbt_graph_dev = my_dbt_graph.to_job(
+        resource_defs={
+            "dbt": dbt_cli_resource.configured(
+                {"project_dir": "path/to/dbt/project", "profile": "dev"}
+            )
+        }
+    )
+
+    my_dbt_graph_prod = my_dbt_graph.to_job(
+        resource_defs={
+            "dbt": dbt_cli_resource.configured(
+                {"project_dir": "path/to/dbt/project", "profile": "prod"}
+            )
+        }
+    )
 
     # end_marker_dbt_cli_profile_modes
 
 
-def scope_dbt_cli_run_after_another_solid():
-    # start_marker_dbt_cli_run_after_another_solid
-    from dagster import pipeline, solid, ModeDefinition
+def scope_dbt_cli_run_after_another_op():
+    # start_marker_dbt_cli_run_after_another_op
+    from dagster import graph, op
     from dagster_dbt import dbt_cli_resource, DbtCliOutput
 
     my_dbt_resource = dbt_cli_resource.configured({"project_dir": "path/to/dbt/project"})
 
-    @solid(required_resource_keys={"dbt"})
+    @op(required_resource_keys={"dbt"})
     def run_models(context) -> DbtCliOutput:
         return context.resources.dbt.run()
 
-    @solid(required_resource_keys={"dbt"})
+    @op(required_resource_keys={"dbt"})
     def test_models(context, run_result: DbtCliOutput):
         context.log.info(f"testing result of `{run_result.command}`!")
         context.resources.dbt.test()
 
-    @pipeline(mode_defs=[ModeDefinition(resource_defs={"dbt": my_dbt_resource})])
-    def my_dbt_pipeline():
+    @graph
+    def my_dbt_graph():
         run_result = run_models()
         test_models(run_result)
 
-    # end_marker_dbt_cli_run_after_another_solid
+    my_dbt_job = my_dbt_graph.to_job(resource_defs={"dbt": my_dbt_resource})
+
+    # end_marker_dbt_cli_run_after_another_op
 
 
 def scope_dbt_rpc_resource():
@@ -136,13 +139,14 @@ def scope_dbt_rpc_run():
     my_remote_rpc = dbt_rpc_resource.configured({"host": "80.80.80.80", "port": 8080})
 
     # start_marker_dbt_rpc_run
-    from dagster import ModeDefinition, pipeline
+    from dagster import graph
     from dagster_dbt import dbt_rpc_run
 
-    @pipeline(mode_defs=[ModeDefinition(resource_defs={"dbt_rpc": my_remote_rpc})])
-    def my_dbt_pipeline():
+    @graph
+    def my_dbt_graph():
         dbt_rpc_run()
 
+    my_dbt_job = my_dbt_graph.to_job(resource_defs={"dbt_rpc": my_remote_rpc})
     # end_marker_dbt_rpc_run
 
 
@@ -151,7 +155,7 @@ def scope_dbt_rpc_run_specific_models():
 
     my_remote_rpc = dbt_rpc_resource.configured({"host": "80.80.80.80", "port": 8080})
     # start_marker_dbt_rpc_run_specific_models
-    from dagster import ModeDefinition, pipeline
+    from dagster import graph
     from dagster_dbt import dbt_rpc_run
 
     run_staging_models = dbt_rpc_run.configured(
@@ -159,10 +163,11 @@ def scope_dbt_rpc_run_specific_models():
         name="run_staging_models",
     )
 
-    @pipeline(mode_defs=[ModeDefinition(resource_defs={"dbt_rpc": my_remote_rpc})])
-    def my_dbt_pipeline():
+    @graph
+    def my_dbt_graph():
         run_staging_models()
 
+    my_dbt_job = my_dbt_graph.to_job(resource_defs={"dbt_rpc": my_remote_rpc})
     # end_marker_dbt_rpc_run_specific_models
 
 
@@ -171,12 +176,14 @@ def scope_dbt_rpc_run_and_wait():
 
     my_remote_rpc = dbt_rpc_resource.configured({"host": "80.80.80.80", "port": 8080})
     # start_marker_dbt_rpc_run_and_wait
-    from dagster import ModeDefinition, pipeline
+    from dagster import graph
     from dagster_dbt import dbt_rpc_run_and_wait
 
-    @pipeline(mode_defs=[ModeDefinition(resource_defs={"dbt_rpc": my_remote_rpc})])
-    def my_dbt_pipeline():
+    @graph
+    def my_dbt_graph():
         dbt_rpc_run_and_wait()
+
+    my_dbt_job = my_dbt_graph.to_job(resource_defs={"dbt_rpc": my_remote_rpc})
 
     # end_marker_dbt_rpc_run_and_wait
 
@@ -185,82 +192,82 @@ def scope_dbt_cli_config_profile_and_target():
     PROFILE_NAME, TARGET_NAME = "", ""
 
     # start_marker_dbt_cli_config_profile_and_target
-    from dagster import pipeline, ModeDefinition
+    from dagster import graph
     from dagster_dbt import dbt_cli_resource
 
     config = {"profile": PROFILE_NAME, "target": TARGET_NAME}
 
-    @pipeline(
-        mode_defs=[ModeDefinition(resource_defs={"dbt": dbt_cli_resource.configured(config)})]
-    )
-    def my_pipeline():
+    @graph
+    def my_graph():
         # ...
         # end_marker_dbt_cli_config_profile_and_target
         pass
 
+    my_job = my_graph.to_job(resource_defs={"dbt": dbt_cli_resource.configured(config)})
+
 
 def scope_dbt_cli_config_executable():
     # start_marker_dbt_cli_config_executable
-    from dagster import pipeline, ModeDefinition
+    from dagster import graph
     from dagster_dbt import dbt_cli_resource
 
     config = {"dbt_executable": "path/to/dbt/executable"}
 
-    @pipeline(
-        mode_defs=[ModeDefinition(resource_defs={"dbt": dbt_cli_resource.configured(config)})]
-    )
-    def my_pipeline():
+    @graph
+    def my_graph():
         # ...
         # end_marker_dbt_cli_config_executable
         pass
 
+    my_job = my_graph.to_job(resource_defs={"dbt": dbt_cli_resource.configured(config)})
+
 
 def scope_dbt_cli_config_select_models():
     # start_marker_dbt_cli_config_select_models
-    from dagster import pipeline, ModeDefinition
+    from dagster import graph
     from dagster_dbt import dbt_cli_resource
 
     config = {"models": ["my_dbt_model+", "path.to.models", "tag:nightly"]}
 
-    @pipeline(
-        mode_defs=[ModeDefinition(resource_defs={"dbt": dbt_cli_resource.configured(config)})]
-    )
-    def my_pipeline():
+    @graph
+    def my_graph():
         # ...
         # end_marker_dbt_cli_config_select_models
         pass
 
+    my_job = my_graph.to_job(resource_defs={"dbt": dbt_cli_resource.configured(config)})
+
 
 def scope_dbt_cli_config_exclude_models():
     # start_marker_dbt_cli_config_exclude_models
-    from dagster import pipeline, ModeDefinition
+    from dagster import graph
     from dagster_dbt import dbt_cli_resource
 
     config = {"exclude": ["my_dbt_model+", "path.to.models", "tag:nightly"]}
 
-    @pipeline(
-        mode_defs=[ModeDefinition(resource_defs={"dbt": dbt_cli_resource.configured(config)})]
-    )
-    def my_pipeline():
+    @graph
+    def my_graph():
         # ...
         # end_marker_dbt_cli_config_exclude_models
         pass
 
+    my_job = my_graph.to_job(resource_defs={"dbt": dbt_cli_resource.configured(config)})
+
 
 def scope_dbt_cli_config_vars():
     # start_marker_dbt_cli_config_vars
-    from dagster import pipeline, ModeDefinition
+    from dagster import graph
     from dagster_dbt import dbt_cli_resource
 
     config = {"vars": {"key": "value"}}
 
-    @pipeline(
-        mode_defs=[ModeDefinition(resource_defs={"dbt": dbt_cli_resource.configured(config)})]
-    )
-    def my_pipeline():
+    @graph
+    def my_graph():
         # ...
         # end_marker_dbt_cli_config_vars
         pass
+
+    my_job = my_graph.to_job(resource_defs={"dbt": dbt_cli_resource.configured(config)})
 
 
 def scope_dbt_rpc_resource_example():
@@ -278,7 +285,7 @@ def scope_dbt_rpc_config_select_models():
 
     from dagster_dbt import dbt_rpc_run
 
-    custom_solid = dbt_rpc_run.configured(config, name="custom_solid")
+    custom_op = dbt_rpc_run.configured(config, name="custom_op")
     # end_marker_dbt_rpc_config_select_models
 
 
@@ -288,7 +295,7 @@ def scope_dbt_rpc_config_exclude_models():
 
     from dagster_dbt import dbt_rpc_run
 
-    custom_solid = dbt_rpc_run.configured(config, name="custom_solid")
+    custom_op = dbt_rpc_run.configured(config, name="custom_op")
     # end_marker_dbt_rpc_config_exclude_models
 
 
@@ -298,7 +305,7 @@ def scope_dbt_rpc_and_wait_config_polling_interval():
 
     from dagster_dbt import dbt_rpc_run
 
-    custom_solid = dbt_rpc_run.configured(config, name="custom_solid")
+    custom_op = dbt_rpc_run.configured(config, name="custom_op")
     # end_marker_dbt_rpc_and_wait_config_polling_interval
 
 
@@ -308,5 +315,5 @@ def scope_dbt_rpc_config_disable_assets():
 
     from dagster_dbt import dbt_rpc_run
 
-    custom_solid = dbt_rpc_run.configured(config, name="custom_solid")
+    custom_op = dbt_rpc_run.configured(config, name="custom_op")
     # end_marker_dbt_rpc_config_disable_assets
